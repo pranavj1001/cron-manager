5d5e9749693a49d3af0d09776823be91e75210a5
Pranav Jain <pranavj1001@gmail.com>
2023-04-26 01:32:14 +0530

    declare necessary apis

---
+++ backend/server/index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const fs = require("fs");
+const path = require("path");
+const util = require("util");
+
+const port = 3691;
+const SUCCESS_HTTP_CODE = 200;
+const SERVER_ERROR_CODE = 500;
+const BAD_REQ_ERROR_CODE = 400;
+const successResponse = {
+  status: SUCCESS_HTTP_CODE,
+  statusMessage: "OK",
+  resp: {},
+};
+const errorResponse = {
+  status: SERVER_ERROR_CODE,
+  statusMessage: "Error",
+  resp: {},
+};
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+const getAndPrintErrorString = (url, error) => {
+  const errorString = `Exception occurred at ${url}, Details \n ${util.inspect(
+    error
+  )}`;
+  console.error(errorString);
+  return errorString;
+};
+
+app.post("/cron", (req, res) => {
+  res.sendStatus(501);
+});
+
+app.get("/listCrons", (req, res) => {
+  res.sendStatus(501);
+});
+
+app.delete("/cron", (req, res) => {
+  res.sendStatus(501);
+});
+
+app.get("/", (req, res) => {
+  res
+    .status(SUCCESS_HTTP_CODE)
+    .json({ ...successResponse, resp: "HEALTH OK!" });
+});
+
+app.listen(port, () => {
+  console.log(`cron-manager backend server initialized on port ${port}`);
+});
