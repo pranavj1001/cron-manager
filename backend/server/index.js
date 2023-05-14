587d461141a8a9e7a465e8599934454da567abcb
Pranav Jain <pranavj1001@gmail.com>
2023-05-15 00:05:00 +0530

    add complete logic for POST /cron API

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fileUpload = require('express-fileupload');
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
@@ -8,7 +9,14 @@ const fs = require("fs");
 const path = require("path");
 const util = require("util");
 
+const {
+  restartCronfig,
+  appendCronfig,
+  loadUpCrons
+} = require('./cron-config');
+
 const cronsFolder = path.join(__dirname, '../crons');
+const tmpFolder = path.join(__dirname, '../.tmp');
 
 const port = 3691;
 const SUCCESS_HTTP_CODE = 200;
@@ -26,6 +34,12 @@ const errorResponse = {
 };
 
 const app = express();
+app.use(fileUpload({
+  useTempFiles : true,
+  tempFileDir : tmpFolder,
+	createParentPath: true,
+	limits: { fileSize: 50 * 1024 * 1024 }
+}));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -41,14 +55,67 @@ const getAndPrintErrorString = (url, error) => {
 
 app.post("/cron", (req, res) => {
   try {
-    cron.schedule("*/5 * * * * *", () => {
-      const cp = require("child_process");
-      cp.execSync(`node ${path.join(cronsFolder, './cron1.js')}`);
-    });
 
-    res.status(SUCCESS_HTTP_CODE).json({...successResponse, resp: `cron created.`});
+    console.log(req.files);
+
+    // check the request body
+		if (
+			!req.files ||
+			Object.keys(req.files).length === 0 ||
+			!req.files["crons"]
+		) {
+			res.status(BAD_REQ_ERROR_CODE).json({...errorResponse, status: BAD_REQ_ERROR_CODE, resp: `Crons not found. No files were uploaded.`});
+      return;
+		}
+
+    if (!req.body || !req.body.expression || !req.body.cronFileName || !req.body.name) {
+      res.status(BAD_REQ_ERROR_CODE).json({...errorResponse, status: BAD_REQ_ERROR_CODE, resp: `Request Body is not in proper format`});
+      return;
+    }
+
+    // save the cron in fs
+    let files = [];
+		if (Array.isArray(req.files["crons"])) {
+			console.log("multiple files upload");
+			files = req.files["crons"];
+		} else {
+			console.log("single file upload");
+			files.push(req.files["crons"]);
+		}
+
+    let cronFileName = "";
+		for (const file of files) {
+			console.log(file);
+			const uploadPath = `${cronsFolder}/${file.name}`;
+      cronFileName = file.name;
+			file.mv(uploadPath, (err) => {
+				if (err) {
+					res.status(SERVER_ERROR_CODE).json({...errorResponse, resp: `Error occurred while uploading ${file.name} on ${uploadPath}`});
+					return;
+				}
+				console.log(`File: ${file.name} was successfully uploaded on ${uploadPath}`);
+			});
+
+      // schedule the cron
+      const requestBody = req.body;
+      cron.schedule(requestBody.expression, () => {
+      const cp = require("child_process");
+        cp.execSync(`node ${path.join(cronsFolder, cronFileName)}`);
+      }, { name: requestBody.name });
+
+      // save the cron config
+      [statusCode, statusResponse] = appendCronfig(requestBody.expression, file.name, { name: requestBody.name });
+
+      if (statusCode === 1) {
+        res.status(SERVER_ERROR_CODE).json({...errorResponse, resp: statusResponse});
+        return;
+      }
+		}
+
+    res.status(SUCCESS_HTTP_CODE).json({...successResponse, resp: `Cron Successfully Created`});
 	} catch(e) {
 		res.status(SERVER_ERROR_CODE).json({...errorResponse, resp: getAndPrintErrorString(req.url, e)});
+    restartCronfig();
 	}
 });
 
